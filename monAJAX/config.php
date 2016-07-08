35abe0ae6f4ffebab6a0e15da7faf1f4fd135efc
book777 <book777@users.noreply.github.com>
2016-07-08 19:36:58 +0200

    v. 1.5.1

--- monAJAX/config.php
+++ monAJAX/config.php
@@ -1,28 +1,29 @@
 <?php
+/*
+ * Version 1.5.1
+ * Autors: @CyberOwl, @MattRh, @book777, @Artmoneyse
+ */
 
-/*			НЕЛЬЗЯ ДАВАТЬ СЕРВЕРАМ ОДИНАКОВЫЕ НАЗВАНИЯ!		(стандартный порт 25565 можно не прописывать)	*/
-$servers[]  = array('address' => '37.59.30.23:22222', 'name' => 'тесt');
-$servers[]  = array('address' => '148.251.80.9:1337', 'name' => 'test1');
-$servers[]  = array('address' => '148.251.125.2', 'name' => 'test2');
-$servers[]  = array('address' => '148.251.80.9:1356', 'name' => 'test3');
-$servers[]  = array('address' => 'zxczxcz.xxx:22222', 'name' => 'test4');
-$servers[]  = array('address' => '37.59.30.23:22222', 'name' => 'HiTech');
-$servers[]  = array('address' => 'play.gmine.ru', 'name' => 'TechnoMagic');
-$servers[]  = array('address' => 'MC.SKYMINE.SU:20850', 'name' => 'MagicRPG');
-$servers[]  = array('address' => '87.98.146.38', 'name' => 'KvidoomRPG');
+/*	Нельзя давать серверам одинаковые имена (name)!	*/
+$servers[]  = array('address' => 'cooll.pp.ua', 'name' => 'тесt');
+$servers[]  = array('address' => '91.121.40.6:25887', 'name' => 'test1');
+//$servers[]  = array('address' => '178.33.4.204:25739', 'name' => 'test2');
+//$servers[]  = array('address' => 'zxczxcz.xxx:22222', 'name' => 'test4');
 
 
-$config = array(
+$config = array
+(
+	'template' => './template/Default/monitoring.tpl',// Путь к папке с шаблоном сайта относительно папки скрипта. Нужен для cron
+	'json_mode' => false, // true - обращение идет напрямую к php. false - к его кэшированной части (потребуется подключить к cron ajax.php)
 
-	'template' => '../templates/Default/monitoring.tpl',// Путь к папке с шаблоном сайта относительно папки скрипта. Нужен для cron-версии
-
-	'tphp' => true,// true - обращение идет к обновляемому json'у. false - к его кэшированной части (потребуется подключить к крон ajax.php)
-	'timecache' => 20,// Промежуток времени (в секундах) через который мониторинг будет обновляться
-	'json' => 'cache/cache.json',// Файл кэширования информации серверов	
-	'timeout' => 3,// Время ожидания ответа сервера майнкрафт
-	'smoothBar' => true,// Плавное появление полосы
-	'sErr' => 'Ошибка..',// Отвечает браузер, если не может считать кэш
-	'dir' => 'http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']).'/'// Путь к скрипту (лучше не менять)
+	'timecache' => 20, // Промежуток времени (в секундах) через который мониторинг будет обновляться
+	'timeout' => 2, // Максимально время на ожидание ответа сервера майнкрафт
+	'smoothBar' => true, // Плавное появление полосы
+	'sErr' => 'Ошибка..', // Ответит браузер, если не сможет считать кэш
+	'dir' => 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/', // Путь к скрипту (лучше не менять)
+	'debug' => false // Отладка в случае проблем https://github.com/book777/monAJAX/issues
 );
-#	date_default_timezone_set('Europe/Moscow');// http://php.net/manual/ru/timezones.php
-?>
+
+// Укажите свою местность, чтобы в рекорд записывалось правильное время
+// http://php.net/manual/ru/timezones.php
+date_default_timezone_set('Europe/Moscow');
